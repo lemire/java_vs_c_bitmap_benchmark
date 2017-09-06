b553113675a4da58f7201070d581ac8bcd739155
Daniel Lemire <lemire@gmail.com>
2017-09-05 23:06:03 -0400

    Adding warm-ups to C/C++

--- c/src/ewah32_benchmarks.cpp
+++ c/src/ewah32_benchmarks.cpp
@@ -2,18 +2,18 @@
 #define _GNU_SOURCE
 #endif
 #define __STDC_FORMAT_MACROS 1
+#include <algorithm>
 #include <inttypes.h>
 #include <iostream>
-#include <algorithm>
-#include <vector>
 #include <list>
+#include <vector>
 #ifdef __cplusplus
 extern "C" {
 #endif
+#include "numbersfromtextfiles.h"
 #include <inttypes.h>
 #include <stdio.h>
 #include <sys/time.h>
-#include "numbersfromtextfiles.h"
 #ifdef __cplusplus
 }
 #endif
@@ -23,138 +23,165 @@ extern "C" {
 /**
  * Once you have collected all the integers, build the bitmaps.
  */
-static std::vector<EWAHBoolArray<uint32_t> > create_all_bitmaps(size_t *howmany,
-        uint32_t **numbers, size_t count) {
-    if (numbers == NULL) return std::vector<EWAHBoolArray<uint32_t> >();
-    std::vector<EWAHBoolArray<uint32_t> > answer(count);
-    for (size_t i = 0; i < count; i++) {
-        EWAHBoolArray<uint32_t> & bm = answer[i];
-        uint32_t * mynumbers = numbers[i];
-        for(size_t j = 0; j < howmany[i] ; ++j) {
-            bm.set(mynumbers[j]);
-        }
-        bm.trim();
+static std::vector<EWAHBoolArray<uint32_t>>
+create_all_bitmaps(size_t *howmany, uint32_t **numbers, size_t count) {
+  if (numbers == NULL)
+    return std::vector<EWAHBoolArray<uint32_t>>();
+  std::vector<EWAHBoolArray<uint32_t>> answer(count);
+  for (size_t i = 0; i < count; i++) {
+    EWAHBoolArray<uint32_t> &bm = answer[i];
+    uint32_t *mynumbers = numbers[i];
+    for (size_t j = 0; j < howmany[i]; ++j) {
+      bm.set(mynumbers[j]);
     }
-    return answer;
+    bm.trim();
+  }
+  return answer;
 }
 
 static void printusage(char *command) {
-    printf(
-        " Try %s directory \n where directory could be "
-        "benchmarks/realdata/census1881\n",
-        command);
-    ;
-    printf("the -v flag turns on verbose mode");
-
+  printf(" Try %s directory \n where directory could be "
+         "benchmarks/realdata/census1881\n",
+         command);
+  ;
+  printf("the -v flag turns on verbose mode");
 }
 
 int main(int argc, char **argv) {
-    int c;
-    uint64_t successive_and = 0;
-    uint64_t successive_or = 0;
-    uint64_t total_or = 0;
+  int c;
+  uint64_t successive_and = 0;
+  uint64_t successive_or = 0;
+  uint64_t total_or = 0;
 
-    const char *extension = ".txt";
-    bool verbose = false;
-    while ((c = getopt(argc, argv, "ve:h")) != -1) switch (c) {
-        case 'e':
-            extension = optarg;
-            break;
-        case 'v':
-            verbose = true;
-            break;
-        case 'h':
-            printusage(argv[0]);
-            return 0;
-        default:
-            abort();
-        }
-    if (optind >= argc) {
-        printusage(argv[0]);
-        return -1;
+  const char *extension = ".txt";
+  bool verbose = false;
+  while ((c = getopt(argc, argv, "ve:h")) != -1)
+    switch (c) {
+    case 'e':
+      extension = optarg;
+      break;
+    case 'v':
+      verbose = true;
+      break;
+    case 'h':
+      printusage(argv[0]);
+      return 0;
+    default:
+      abort();
     }
-    char *dirname = argv[optind];
-    size_t count;
+  if (optind >= argc) {
+    printusage(argv[0]);
+    return -1;
+  }
+  char *dirname = argv[optind];
+  size_t count;
 
-    size_t *howmany = NULL;
-    uint32_t **numbers =
-        read_all_integer_files(dirname, extension, &howmany, &count);
-    if (numbers == NULL) {
-        printf(
-            "I could not find or load any data file with extension %s in "
-            "directory %s.\n",
-            extension, dirname);
-        return -1;
-    }
-    uint32_t maxvalue = 0;
-    for (size_t i = 0; i < count; i++) {
-      if( howmany[i] > 0 ) {
-        if(maxvalue < numbers[i][howmany[i]-1]) {
-           maxvalue = numbers[i][howmany[i]-1];
-         }
+  size_t *howmany = NULL;
+  uint32_t **numbers =
+      read_all_integer_files(dirname, extension, &howmany, &count);
+  if (numbers == NULL) {
+    printf("I could not find or load any data file with extension %s in "
+           "directory %s.\n",
+           extension, dirname);
+    return -1;
+  }
+  uint32_t maxvalue = 0;
+  for (size_t i = 0; i < count; i++) {
+    if (howmany[i] > 0) {
+      if (maxvalue < numbers[i][howmany[i] - 1]) {
+        maxvalue = numbers[i][howmany[i] - 1];
       }
     }
-    uint64_t totalcard = 0;
-    for (size_t i = 0; i < count; i++) {
-      totalcard += howmany[i];
-    }
-    uint64_t successivecard = 0;
-    for (size_t i = 1; i < count; i++) {
-       successivecard += howmany[i-1] + howmany[i];
-    }
+  }
+  uint64_t totalcard = 0;
+  for (size_t i = 0; i < count; i++) {
+    totalcard += howmany[i];
+  }
+  uint64_t successivecard = 0;
+  for (size_t i = 1; i < count; i++) {
+    successivecard += howmany[i - 1] + howmany[i];
+  }
 
-    std::vector<EWAHBoolArray<uint32_t> > bitmaps = create_all_bitmaps(howmany, numbers, count);
-    if (bitmaps.empty()) return -1;
-    uint64_t totalsize = 0;
+  std::vector<EWAHBoolArray<uint32_t>> bitmaps =
+      create_all_bitmaps(howmany, numbers, count);
+  if (bitmaps.empty())
+    return -1;
+  uint64_t totalsize = 0;
 
-    for (int i = 0; i < (int) count; ++i) {
-        EWAHBoolArray<uint32_t> & bv = bitmaps[i];
-        totalsize += bv.sizeInBytes(); // should be close enough to memory usage
-    }
-    struct timeval st, et;
-    int elapsed;
-    gettimeofday(&st,NULL);
+  for (int i = 0; i < (int)count; ++i) {
+    EWAHBoolArray<uint32_t> &bv = bitmaps[i];
+    totalsize += bv.sizeInBytes(); // should be close enough to memory usage
+  }
+  int warmup = 5;
+
+  for (int w = 0; w < warmup; w++) {
     for (int i = 0; i < (int)count - 1; ++i) {
-        EWAHBoolArray<uint32_t>  tempand;
-        bitmaps[i].logicaland(bitmaps[i + 1],tempand);
-        successive_and += tempand.numberOfOnes();
+      EWAHBoolArray<uint32_t> tempand;
+      bitmaps[i].logicaland(bitmaps[i + 1], tempand);
+      successive_and += tempand.numberOfOnes();
     }
-    gettimeofday(&et,NULL);
-    elapsed = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);
-    printf("Successive intersections took %d mus\n", elapsed);
-
-    gettimeofday(&st,NULL);
+  }
+  struct timeval st, et;
+  int elapsed;
+  gettimeofday(&st, NULL);
+  for (int i = 0; i < (int)count - 1; ++i) {
+    EWAHBoolArray<uint32_t> tempand;
+    bitmaps[i].logicaland(bitmaps[i + 1], tempand);
+    successive_and += tempand.numberOfOnes();
+  }
+  gettimeofday(&et, NULL);
+  elapsed = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);
+  printf("Successive intersections took %d mus\n", elapsed);
+  for (int w = 0; w < warmup; w++) {
     for (int i = 0; i < (int)count - 1; ++i) {
-        EWAHBoolArray<uint32_t>  tempor;
-        bitmaps[i].logicalor(bitmaps[i + 1],tempor);
-        successive_or += tempor.numberOfOnes();
+      EWAHBoolArray<uint32_t> tempor;
+      bitmaps[i].logicalor(bitmaps[i + 1], tempor);
+      successive_or += tempor.numberOfOnes();
     }
-    gettimeofday(&et,NULL);
-    elapsed = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);
+  }
+  gettimeofday(&st, NULL);
+  for (int i = 0; i < (int)count - 1; ++i) {
+    EWAHBoolArray<uint32_t> tempor;
+    bitmaps[i].logicalor(bitmaps[i + 1], tempor);
+    successive_or += tempor.numberOfOnes();
+  }
+  gettimeofday(&et, NULL);
+  elapsed = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);
 
-    printf("Successive unions took %d mus\n", elapsed);
-
-    gettimeofday(&st,NULL);
-    if(count>1) {
-        EWAHBoolArray<uint32_t>  totalorbitmap;
-        bitmaps[0].logicalor(bitmaps[1],totalorbitmap);
-        for(int i = 2 ; i < (int) count; ++i) {
-          EWAHBoolArray<uint32_t>  tmp;
-          totalorbitmap.logicalor(bitmaps[i],tmp);
-          tmp.swap(totalorbitmap);
-        }
-        total_or = totalorbitmap.numberOfOnes();
+  printf("Successive unions took %d mus\n", elapsed);
+  for (int w = 0; w < warmup; w++) {
+    if (count > 1) {
+      EWAHBoolArray<uint32_t> totalorbitmap;
+      bitmaps[0].logicalor(bitmaps[1], totalorbitmap);
+      for (int i = 2; i < (int)count; ++i) {
+        EWAHBoolArray<uint32_t> tmp;
+        totalorbitmap.logicalor(bitmaps[i], tmp);
+        tmp.swap(totalorbitmap);
+      }
+      total_or = totalorbitmap.numberOfOnes();
     }
-    gettimeofday(&et,NULL);
-    elapsed = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);
-    printf("Total unions took %d mus\n", elapsed);
-
-    for (int i = 0; i < (int)count; ++i) {
-        free(numbers[i]);
-        numbers[i] = NULL;  // paranoid
+  }
+  gettimeofday(&st, NULL);
+  if (count > 1) {
+    EWAHBoolArray<uint32_t> totalorbitmap;
+    bitmaps[0].logicalor(bitmaps[1], totalorbitmap);
+    for (int i = 2; i < (int)count; ++i) {
+      EWAHBoolArray<uint32_t> tmp;
+      totalorbitmap.logicalor(bitmaps[i], tmp);
+      tmp.swap(totalorbitmap);
     }
-    free(howmany);
-    free(numbers);
+    total_or = totalorbitmap.numberOfOnes();
+  }
+  gettimeofday(&et, NULL);
+  elapsed = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);
+  printf("Total unions took %d mus\n", elapsed);
 
-    return 0;
+  for (int i = 0; i < (int)count; ++i) {
+    free(numbers[i]);
+    numbers[i] = NULL; // paranoid
+  }
+  free(howmany);
+  free(numbers);
+
+  return 0;
 }
--- c/src/roaring_benchmarks.c
+++ c/src/roaring_benchmarks.c
@@ -5,165 +5,195 @@
 #include "cmemcounter.h"
 #endif
 
+#include "numbersfromtextfiles.h"
+#include "roaring.c"
 #include <inttypes.h>
 #include <stdio.h>
 #include <sys/time.h>
-#include "numbersfromtextfiles.h"
-#include "roaring.c"
 
 bool roaring_iterator_increment(uint32_t value, void *param) {
-    size_t count;
-    memcpy(&count, param, sizeof(uint64_t));
-    count++;
-    memcpy(param, &count, sizeof(uint64_t));
-    (void) value;
-    return true;  // continue till the end
+  size_t count;
+  memcpy(&count, param, sizeof(uint64_t));
+  count++;
+  memcpy(param, &count, sizeof(uint64_t));
+  (void)value;
+  return true; // continue till the end
 }
 
 /**
  * Once you have collected all the integers, build the bitmaps.
  */
 static roaring_bitmap_t **create_all_bitmaps(size_t *howmany,
-        uint32_t **numbers, size_t count, bool runoptimize, bool copyonwrite, bool verbose, uint64_t * totalsize) {
-    *totalsize = 0;
-    if (numbers == NULL) return NULL;
-    size_t savedmem = 0;
-    roaring_bitmap_t **answer = malloc(sizeof(roaring_bitmap_t *) * count);
-    for (size_t i = 0; i < count; i++) {
-        answer[i] = roaring_bitmap_of_ptr(howmany[i], numbers[i]);
-        answer[i]->copy_on_write = copyonwrite;
-        if(runoptimize) roaring_bitmap_run_optimize(answer[i]);
-        savedmem += roaring_bitmap_shrink_to_fit(answer[i]);
-        *totalsize += roaring_bitmap_portable_size_in_bytes(answer[i]);
-    }
-    if(verbose) printf("saved bytes by shrinking : %zu \n",savedmem);
-    return answer;
+                                             uint32_t **numbers, size_t count,
+                                             bool runoptimize, bool copyonwrite,
+                                             bool verbose,
+                                             uint64_t *totalsize) {
+  *totalsize = 0;
+  if (numbers == NULL)
+    return NULL;
+  size_t savedmem = 0;
+  roaring_bitmap_t **answer = malloc(sizeof(roaring_bitmap_t *) * count);
+  for (size_t i = 0; i < count; i++) {
+    answer[i] = roaring_bitmap_of_ptr(howmany[i], numbers[i]);
+    answer[i]->copy_on_write = copyonwrite;
+    if (runoptimize)
+      roaring_bitmap_run_optimize(answer[i]);
+    savedmem += roaring_bitmap_shrink_to_fit(answer[i]);
+    *totalsize += roaring_bitmap_portable_size_in_bytes(answer[i]);
+  }
+  if (verbose)
+    printf("saved bytes by shrinking : %zu \n", savedmem);
+  return answer;
 }
 
 static void printusage(char *command) {
-    printf(
-        " Try %s directory \n where directory could be "
-        "benchmarks/realdata/census1881\n",
-        command);
-    ;
-    printf("the -r flag turns on run optimization");
+  printf(" Try %s directory \n where directory could be "
+         "benchmarks/realdata/census1881\n",
+         command);
+  ;
+  printf("the -r flag turns on run optimization");
 }
 
-
 int main(int argc, char **argv) {
-    int c;
-    bool runoptimize = true;
-    bool verbose = false;
-    uint64_t successive_and = 0;
-    uint64_t successive_or = 0;
-    uint64_t total_or = 0;
+  int c;
+  bool runoptimize = true;
+  bool verbose = false;
+  uint64_t successive_and = 0;
+  uint64_t successive_or = 0;
+  uint64_t total_or = 0;
 
-    bool copyonwrite = false;
-    char *extension = ".txt";
-    while ((c = getopt(argc, argv, "cvre:h")) != -1) switch (c) {
-        case 'e':
-            extension = optarg;
-            break;
-        case 'v':
-            verbose = true;
-            break;
-        case 'r':
-            runoptimize = true;
-            if(verbose) printf("enabling run optimization\n");
-            break;
-        case 'c':
-            copyonwrite = true;
-            if(verbose) printf("enabling copyonwrite\n");
-            break;
-        case 'h':
-            printusage(argv[0]);
-            return 0;
-        default:
-            abort();
-        }
-    if (optind >= argc) {
-        printusage(argv[0]);
-        return -1;
+  bool copyonwrite = false;
+  char *extension = ".txt";
+  while ((c = getopt(argc, argv, "cvre:h")) != -1)
+    switch (c) {
+    case 'e':
+      extension = optarg;
+      break;
+    case 'v':
+      verbose = true;
+      break;
+    case 'r':
+      runoptimize = true;
+      if (verbose)
+        printf("enabling run optimization\n");
+      break;
+    case 'c':
+      copyonwrite = true;
+      if (verbose)
+        printf("enabling copyonwrite\n");
+      break;
+    case 'h':
+      printusage(argv[0]);
+      return 0;
+    default:
+      abort();
     }
-    char *dirname = argv[optind];
-    size_t count;
+  if (optind >= argc) {
+    printusage(argv[0]);
+    return -1;
+  }
+  char *dirname = argv[optind];
+  size_t count;
 
-    size_t *howmany = NULL;
-    uint32_t **numbers =
-        read_all_integer_files(dirname, extension, &howmany, &count);
-    if (numbers == NULL) {
-        printf(
-            "I could not find or load any data file with extension %s in "
-            "directory %s.\n",
-            extension, dirname);
-        return -1;
+  size_t *howmany = NULL;
+  uint32_t **numbers =
+      read_all_integer_files(dirname, extension, &howmany, &count);
+  if (numbers == NULL) {
+    printf("I could not find or load any data file with extension %s in "
+           "directory %s.\n",
+           extension, dirname);
+    return -1;
+  }
+  uint32_t maxvalue = 0;
+  for (size_t i = 0; i < count; i++) {
+    if (howmany[i] > 0) {
+      if (maxvalue < numbers[i][howmany[i] - 1]) {
+        maxvalue = numbers[i][howmany[i] - 1];
+      }
     }
-    uint32_t maxvalue = 0;
-    for (size_t i = 0; i < count; i++) {
-        if( howmany[i] > 0 ) {
-            if(maxvalue < numbers[i][howmany[i]-1]) {
-                maxvalue = numbers[i][howmany[i]-1];
-            }
-        }
-    }
-    uint64_t totalcard = 0;
-    for (size_t i = 0; i < count; i++) {
-        totalcard += howmany[i];
-    }
-    uint64_t successivecard = 0;
-    for (size_t i = 1; i < count; i++) {
-        successivecard += howmany[i-1] + howmany[i];
-    }
+  }
+  uint64_t totalcard = 0;
+  for (size_t i = 0; i < count; i++) {
+    totalcard += howmany[i];
+  }
+  uint64_t successivecard = 0;
+  for (size_t i = 1; i < count; i++) {
+    successivecard += howmany[i - 1] + howmany[i];
+  }
 
-    uint64_t totalsize = 0;
-    roaring_bitmap_t **bitmaps = create_all_bitmaps(howmany, numbers, count,runoptimize,copyonwrite, verbose, &totalsize);
-    if (bitmaps == NULL) return -1;
+  uint64_t totalsize = 0;
+  roaring_bitmap_t **bitmaps = create_all_bitmaps(
+      howmany, numbers, count, runoptimize, copyonwrite, verbose, &totalsize);
+  if (bitmaps == NULL)
+    return -1;
+  int warmup = 5;
 
-    struct timeval st, et;
-    int elapsed;
-    gettimeofday(&st,NULL);
+  for (int w = 0; w < warmup; w++) {
     for (int i = 0; i < (int)count - 1; ++i) {
-        roaring_bitmap_t *tempand =
-            roaring_bitmap_and(bitmaps[i], bitmaps[i + 1]);
-        successive_and += roaring_bitmap_get_cardinality(tempand);
-        roaring_bitmap_free(tempand);
+      roaring_bitmap_t *tempand =
+          roaring_bitmap_and(bitmaps[i], bitmaps[i + 1]);
+      successive_and += roaring_bitmap_get_cardinality(tempand);
+      roaring_bitmap_free(tempand);
     }
+  }
 
-    gettimeofday(&et,NULL);
-    elapsed = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);
+  struct timeval st, et;
+  int elapsed;
+  gettimeofday(&st, NULL);
+  for (int i = 0; i < (int)count - 1; ++i) {
+    roaring_bitmap_t *tempand = roaring_bitmap_and(bitmaps[i], bitmaps[i + 1]);
+    successive_and += roaring_bitmap_get_cardinality(tempand);
+    roaring_bitmap_free(tempand);
+  }
 
-    printf("Successive intersections took %d mus\n", elapsed);
+  gettimeofday(&et, NULL);
+  elapsed = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);
 
-    gettimeofday(&st,NULL);
+  printf("Successive intersections took %d mus\n", elapsed);
+
+  for (int w = 0; w < warmup; w++) {
     for (int i = 0; i < (int)count - 1; ++i) {
-        roaring_bitmap_t *tempor =
-            roaring_bitmap_or(bitmaps[i], bitmaps[i + 1]);
-        successive_or += roaring_bitmap_get_cardinality(tempor);
-        roaring_bitmap_free(tempor);
+      roaring_bitmap_t *tempor = roaring_bitmap_or(bitmaps[i], bitmaps[i + 1]);
+      successive_or += roaring_bitmap_get_cardinality(tempor);
+      roaring_bitmap_free(tempor);
     }
-    gettimeofday(&et,NULL);
-    elapsed = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);
+  }
 
-    printf("Successive unions took %d mus\n", elapsed);
+  gettimeofday(&st, NULL);
+  for (int i = 0; i < (int)count - 1; ++i) {
+    roaring_bitmap_t *tempor = roaring_bitmap_or(bitmaps[i], bitmaps[i + 1]);
+    successive_or += roaring_bitmap_get_cardinality(tempor);
+    roaring_bitmap_free(tempor);
+  }
+  gettimeofday(&et, NULL);
+  elapsed = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);
 
-    gettimeofday(&st,NULL);
-    roaring_bitmap_t * totalorbitmap = roaring_bitmap_or_many(count,(const roaring_bitmap_t **)bitmaps);
+  printf("Successive unions took %d mus\n", elapsed);
+  for (int w = 0; w < warmup; w++) {
+    roaring_bitmap_t *totalorbitmap =
+        roaring_bitmap_or_many(count, (const roaring_bitmap_t **)bitmaps);
     total_or = roaring_bitmap_get_cardinality(totalorbitmap);
     roaring_bitmap_free(totalorbitmap);
-    gettimeofday(&et,NULL);
-    elapsed = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);
-    printf("Total unions took %d mus\n", elapsed);
+  }
 
+  gettimeofday(&st, NULL);
+  roaring_bitmap_t *totalorbitmap =
+      roaring_bitmap_or_many(count, (const roaring_bitmap_t **)bitmaps);
+  total_or = roaring_bitmap_get_cardinality(totalorbitmap);
+  roaring_bitmap_free(totalorbitmap);
+  gettimeofday(&et, NULL);
+  elapsed = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);
+  printf("Total unions took %d mus\n", elapsed);
 
-    for (int i = 0; i < (int)count; ++i) {
-        free(numbers[i]);
-        numbers[i] = NULL;  // paranoid
-        roaring_bitmap_free(bitmaps[i]);
-        bitmaps[i] = NULL;  // paranoid
-    }
-    free(bitmaps);
-    free(howmany);
-    free(numbers);
+  for (int i = 0; i < (int)count; ++i) {
+    free(numbers[i]);
+    numbers[i] = NULL; // paranoid
+    roaring_bitmap_free(bitmaps[i]);
+    bitmaps[i] = NULL; // paranoid
+  }
+  free(bitmaps);
+  free(howmany);
+  free(numbers);
 
-    return 0;
+  return 0;
 }
